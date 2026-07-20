87ae0b3ed5f92887ea5ba2e5b8d9c04875667781
otaviobonini <otgbonini@gmail.com>
2026-07-20 11:38:21 -0300

    style: adds new product card and category card style

--- components/Categoria.tsx
+++ components/Categoria.tsx
@@ -15,14 +15,23 @@ export default function Categoria({
   return (
     <Link
       href={`/category/${categoryId}?categoryName=${category}`}
-      className="gap-4 items-center shrink-0 flex flex-col"
+      className="gap-4 items-center hover:-translate-y-1
+shrink-0 flex flex-col group  rounded-lg transition-all duration-300 p-4"
     >
       <Image
         src={img}
         alt={category}
         width={200}
         height={200}
-        className="h-32 w-32 md:h-48 md:w-48 rounded-full object-cover"
+        className="  h-32 w-32
+  md:h-48 md:w-48
+  rounded-full
+  border-2
+  border-gray-200
+  group-hover:border-blue-600
+  transition-colors
+  group-hover:shadow-lg 
+  object-cover"
       />
 
       <h1 className="mt-2 text-center font-medium">{category}</h1>
--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -14,14 +14,16 @@ export default function Product({ img, name, price, productId }: productsType) {
     <Link
       href={`/product/${productId}`}
       id={productId}
-      className="shrink-0 p-4 max-w-fit hover:scale-110 mb-8 mt-4 hover:border hover:shadow-md border border-transparent hover:border-gray-200 rounded-lg transition-all duration-300  flex flex-col "
+      className="shrink-0 group p-4 max-w-fit hover:-translate-y-1
+mb-8 mt-4 border border-gray-200
+  rounded-lg transition-all duration-300  flex flex-col "
     >
       <Image
         src={img}
         width={200}
         height={100}
-        alt=""
-        className="h-48 w-48 rounded-lg lg:h-64 lg:w-64"
+        alt={name}
+        className="md:h-48 md:w-48 w-36 h-36 group-hover:border-blue-600 border-2 border-gray-200 group-hover:shadow-lg rounded-lg lg:h-64 lg:w-64"
       ></Image>
       <h1 className=" text-gray-400 py-4">{name}</h1>
       <p className="text-xl font-bold ">{formatPrice(price)}</p>
